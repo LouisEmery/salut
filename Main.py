3cab58873b90fc493bf12ca14f1517e86b606fcd
Louis Emery <112487894+LouisEmery@users.noreply.github.com>
2023-03-08 09:51:01 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,19 +6,19 @@ import random
 list_color= [arcade.color.ALLOY_ORANGE, arcade.color.AFRICAN_VIOLET, arcade.color.ARMY_GREEN]
 
 class Cercle():
-    def __init__(self,rayon,x,y,color):
+    def __init__(self,x,y, rayon,color):
         self.rayon = rayon
         self.x=x
         self.y=y
         self.color=color
 
     def draw(self):
-        arcade.draw_circle_filled(10, 10, 20, (255, 54, 34))
+        arcade.draw_circle_filled(self.x, self.y, self.rayon, self.color)
 
 class MyGame(arcade.Window):
-    def __init__(self, width, height, title, color):
+    def __init__(self, width, height, title):
        # Call the parent class's init function
-       super().__init__(width, height, title, color)
+       super().__init__(width, height, title)
        self.list_cercle = []
 
     def setup(self):
@@ -28,13 +28,16 @@ class MyGame(arcade.Window):
             rayon = 20
             couleur = random.choice(list_color)
             cercle = Cercle(posx, posy, rayon, couleur)
-            self.liste_cercle.append(cercle)
+            self.list_cercle.append(cercle)
     def on_draw(self):
         arcade.start_render()
+        for cercle in self.list_cercle:
+            cercle.draw()
+
 
 def main():
-    window = MyGame(640, 480, "Drawing Example", (255, 54, 34))
-
+    window = MyGame(640, 480, "Drawing Example")
+    window.setup()
 
     arcade.run()
 
